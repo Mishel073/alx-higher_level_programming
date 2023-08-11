636d96e71248194dad94641ee26aec9267901695
Mishel073 <bprecious477@gmail.com>
2023-08-11 12:45:08 +0000

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 import sys
-def main(argv):
-    total = 0
 
-    for arg in argv:
-        total += int(arg)
-    print(total)
+def main(argv):
+    num_args = len(argv)
+    
+    if num_args == 0:
+        print("Number of argument(s): .")
+    else:
+        if num_args == 1:
+            print("Number of argument(s): 1")
+            print("1:", argv[0])
+        else:
+            print(f"Number of argument(s): {num_args}")
+            for i, arg in enumerate(argv):
+                print(f"{i+1}: {arg}")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
